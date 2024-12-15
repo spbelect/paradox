00010ba04d188bd3e2168b79118ed30a2ca02f00
Evstifeev Roman <someuniquename@gmail.com>
2024-12-15 15:21:31 +0500

    label.py logging

--- label.py
+++ label.py
@@ -1,11 +1,12 @@
 # -*- coding: utf-8 -*-
-from __future__ import unicode_literals
 
 import kivy.uix.label
 from kivy.factory import Factory
 from kivy.lang import Builder
 from kivy.properties import ListProperty
 
+
+from loguru import logger
 
 # NOTE: opened feature request "Move background_color property from Button up to Label"
 # https://github.com/kivy/kivy/issues/6265
@@ -14,9 +15,10 @@ from kivy.properties import ListProperty
 class Label(kivy.uix.label.Label):
     background_color = ListProperty([1, 1, 1, 0])
 
-
 Factory.unregister('Label')
 Factory.register('Label', cls=Label)
+
+logger.info('Registered custom Label widget')
 
 
 Builder.load_string("""
